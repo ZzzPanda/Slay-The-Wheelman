fb99994124ce66a2f73e7a6db5b1ea854f9c55b1
Jay Luo <jie.luo@tubitv.com>
2026-03-13 12:10:16 +0800

    Reapply "Godot: update Android settings and keep screen on"

--- scripts/Root.gd
+++ scripts/Root.gd
@@ -4,4 +4,4 @@ func _ready() -> void:
 	# Enable immersive mode on Android
 	if OS.has_feature("android"):
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-		DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_KEEP_SCREEN_ON, true)
+		DisplayServer.screen_set_keep_on(true)
