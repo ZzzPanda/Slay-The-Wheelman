0fd54ac9a6a3b13ff119302abcd8617a5f8e3017
罗杰 <luojie@qingting.fm>
2026-03-13 10:57:08 +0800

    feat: 添加安卓全屏适配配置
    - project.godot: 添加 display/stretch模式和android安全区域配置
    - scripts/Root.gd: 添加Android沉浸式全屏模式代码

--- scripts/Root.gd
+++ scripts/Root.gd
@@ -1,1 +1,7 @@
 extends Node2D
+
+func _ready() -> void:
+	# Enable immersive mode on Android
+	if OS.has_feature("android"):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_KEEP_SCREEN_ON, true)
